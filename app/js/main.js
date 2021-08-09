2e5aeaced57c8da8c2cf97fdfd99b1b9bb83db1e
aleksei24 <lexa24032@gmail.com>
2021-08-09 16:48:55 +0300

    tabs block has been linked

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,4 @@
 // the code below works well, though errors are underlined
 @@include('./chunks/_hi.js')
 @@include('./chunks/_burger.js')
-@@include('./chunks/_vertical_tabs.js')
+@@include('./chunks/_tabs.js')
