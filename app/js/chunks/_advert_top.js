9b94f0794993793b7a7c32e2f802d7032b094d58
aleksei24 <lexa24032@gmail.com>
2021-09-27 11:59:51 +0300

    got rid of unnessesary items

--- app/js/chunks/_advert_top.js
+++
@@ -1,9 +0,0 @@
-// catalogue top
-
-const catalogueTopCloseBtn = document.querySelector('.top__close');
-
-if (catalogueTopCloseBtn) {
-    catalogueTopCloseBtn.addEventListener('click', (el) => {
-        el.currentTarget.closest('.top').style.display = 'none';
-    });
-}
